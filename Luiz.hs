86f4c8dd3e4a83cb4c2d3db5177d06ff667ac23e
Luiz Willner <luiz.claudio@injunior.com.br>
2022-11-17 14:29:25 -0300

    Função refactorFormulaList feita

--- Luiz.hs
+++ Luiz.hs
@@ -2,11 +2,11 @@ import Data.List
 
 ----------- Functions -----------
 
--- Deletar item de uma lista pelo index
+-- Deleta item de uma lista pelo index
 delByIndex list i = take i list ++ drop (1 + i) list
 
 
--- Deletar primeiro e último elementos de uma lista
+-- Deleta primeiro e último elementos de uma lista
 delInitLast list = delByIndex (delByIndex list 0) ((length list) - 2)
 
 appendElement a [] = [a]
@@ -14,7 +14,7 @@ appendElement a (x:xs) = x : appendElement a xs
 
 
 -- Faz um match da posição de abertura e fechamento de um parêntese
--- ex: "a(bc(d))" -> [(1, 7), (4, 6)]
+-- ex: "a(bc(d))" ----> [(1, 7), (4, 6)]
 parenPairs :: String -> [(Int, Int)]
 parenPairs = go 0 []
   where
@@ -27,21 +27,31 @@ parenPairs = go 0 []
 
 
 -- Pega o primeiro segmento de Parênteses
--- ex: "a(bc(d))" -> "(bc(d)))"
+-- ex: "a(bc(d))" ---> "(bc(d)))"
 firstParenSeg :: String -> String
 firstParenSeg s = f s (minimum (parenPairs s))
   where
     f s (i, j) = take (j - i + 1) (drop i s)
 
 
--- Separar os 2 operandos da string colocando cada um como elemento da lista
--- ex: [">", "(v(b,a)),(v(c,a))"] -> [">", "(v(b,a))", "(v(c,a))"]
+-- Separa os 2 operandos da string colocando cada um como elemento da lista
+-- ex: "(v(b,a)),(v(c,a))" ---> ["(v(b,a))", "(v(c,a))"]
 splitOperands :: String -> [String]
 splitOperands strFormula = [(fst dirtySeparatedOperands), (drop 1 (snd dirtySeparatedOperands))] where 
   dirtySeparatedOperands = splitAt ( (snd (minimum (parenPairs strFormula))) + 1) strFormula
 
 
--- Separar o operador da fórmula do restante dos operandos
--- ex: "(>((v(b,a)),(v(c,a))))" -> [">", "(v(b,a)),(v(c,a))"]
-f str = [(take 1 str2), (delInitLast (tail str2))] where
+-- Refaz a lista que representa fórmula, agora com os operandos separados em cada elemento
+-- ex: [">", "(v(b,a)),(v(c,a))"] ---> [">","(v(b,a))","(v(c,a))"]
+refactorFormulaList :: [String] -> [String]
+refactorFormulaList oldFormulaList = (init oldFormulaList) ++ (splitOperands (oldFormulaList !! 1))
+
+
+-- Separa o operador da fórmula do restante dos operandos
+-- ex: "(>((v(b,a)),(v(c,a))))" ---> [">", "(v(b,a)),(v(c,a))"]
+splitOperator :: [a] -> [[a]]
+splitOperator str = [(take 1 str2), (delInitLast (tail str2))] where
     str2 = delInitLast str
+
+
+--  [">", "(v(b,a)),(v(c,a))"]
